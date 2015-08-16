b7b5711b1ac45d0d7828a2761e3c8ce57615f9fa
unknown <emaddrnawe@gmail>
2015-08-16 19:39:18 +0200

    update test1

--- test/test1.js
+++ test/test1.js
@@ -1,9 +1,51 @@
 var models = require("../models");
 
-var obj_region = {
-	region_name : "بنغازي"
-}
+//// new insert
+// var obj_region = {
+// 	region_name : "مصراته"
+// }
 
-models.Region.create(obj_region).then(function(result) {
+// models.Region.create(obj_region).then(function(result) {
+//   console.log(result);
+// });
+
+
+//// update region_name
+
+// var obj_region = {
+// 	region_name : "الخمس"
+// }
+
+// models.Region.update(obj_region,{
+// 	where:{
+// 		id:2
+// 	}
+// }).then(function(result) {
+//   console.log(result);
+// });
+
+
+//// update status in delete
+
+// var obj_region = {
+// 	status : 0
+// }
+
+// models.Region.update(obj_region,{
+// 	where:{
+// 		id:1
+// 	}
+// }).then(function(result) {
+//   console.log(result);
+// });
+
+////
+
+
+models.Region.destroy({
+	where:{
+		id:1
+	}
+}).then(function(result) {
   console.log(result);
 });
