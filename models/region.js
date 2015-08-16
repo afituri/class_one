2a0e0f8335ce1c3847eca0773f51c5d8bca44838
unknown <emaddrnawe@gmail>
2015-08-16 18:02:38 +0200

    add region

--- models/region.js
+++ models/region.js
@@ -2,7 +2,7 @@
 
 module.exports = function(sequelize, DataTypes) {
   var Region = sequelize.define("Region", {
-    regionname: DataTypes.STRING (60) ,
+    region_name: DataTypes.STRING (60) ,
     status:{type:DataTypes.INTEGER(1),defaultValue:1} 
   }, {
     classMethods: {
