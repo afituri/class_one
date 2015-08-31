e5f7c33bc6599a68f799580b57c8ece17d2cc95e
yakhlef <syokhlif@gmail.com>
2015-08-31 19:26:33 +0200

    cdcfcdasf

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var region = require('./region')(router);
-<<<<<<< HEAD
 var country = require('./country')(router);
-=======
 var manucipality = require('./manucipality')(router);
->>>>>>> 8debfab9c7e778c57065f19c93427a0d44d2acd9
+
 
 
 //manucipality_name
