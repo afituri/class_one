8570ed7535b23def3426c519c3a3ca36a1ae5693
Aladdin <alaaden3000@gmail.com>
2015-09-02 19:22:30 +0200

    add bootstrap notify

--- public/js/region.js
+++ public/js/region.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  
   $("[data-toggle=tooltip]").tooltip();
 
   jQuery.validator.addMethod("arabicLettersOnly", function(value, element) {
@@ -18,7 +19,6 @@ $(document).ready(function(){
         arabicLettersOnly: "الرجاء ادخال حروف عربية فقط!",
       },
     },
-    // errorClass: 'custom-error',
     highlight: function(element) {
       $(element).closest('.row').addClass('has-error');
     },
@@ -40,13 +40,36 @@ $(document).ready(function(){
         arabicLettersOnly: "الرجاء ادخال حروف عربية فقط!",
       },
     },
-    // errorClass: 'custom-error',
     highlight: function(element) {
-      $(element).closest('.row').addClass('has-error');
+      $(element).closest('.row').addClass('has-error').removeClass('has-success');
     },
     unhighlight: function(element) {
-      $(element).closest('.row').removeClass('has-error');
+      $(element).closest('.row').removeClass('has-error').addClass('has-success');
     },
   });
 
+  $("#searchRegion").validate({
+    rules:{
+      searchName:{
+        required: true,
+      },
+    },
+    messages:{
+      searchName:{
+        required: "",
+      },
+    },
+    highlight: function(element) {
+      $(element).closest('.input-group').addClass('has-error').removeClass('has-success');
+    },
+    unhighlight: function(element) {
+      $(element).closest('.input-group').removeClass('has-error').addClass('has-success');
+    },
+    invalidHandler: function(event, validator) {
+      var errors = validator.numberOfInvalids();
+      if (errors) {
+        custNotify("danger","خطأ","الرجاء ادخال كلمة البحث","ok-sign","bounceIn","bounceOut");
+      }
+    },
+  });
 });
