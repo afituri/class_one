ee2c975e2d9500520ea5a70b65cc5de0263a1eaf
Aladdin <alaaden3000@gmail.com>
2015-09-02 18:40:36 +0200

    add libers

---
+++ public/js/region.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+  $("[data-toggle=tooltip]").tooltip();
+
+  jQuery.validator.addMethod("arabicLettersOnly", function(value, element) {
+    return this.optional(element) || /^[أ-ي,ﻻ,ء]+$/i.test(value);
+  }, "الرجاء ادخال حروف عربية فقط!");
+
+  $("#addReginForm").validate({
+    rules:{
+      region_name:{
+        required: true,
+        arabicLettersOnly: true,
+      },
+    },
+    messages:{
+      region_name:{
+        required: "الرجاء ادخال اسم المنطقة!",
+        arabicLettersOnly: "الرجاء ادخال حروف عربية فقط!",
+      },
+    },
+    // errorClass: 'custom-error',
+    highlight: function(element) {
+      $(element).closest('.row').addClass('has-error');
+    },
+    unhighlight: function(element) {
+      $(element).closest('.row').removeClass('has-error');
+    },
+  });
+
+  $("#editReginForm").validate({
+    rules:{
+      region_name:{
+        required: true,
+        arabicLettersOnly: true,
+      },
+    },
+    messages:{
+      region_name:{
+        required: "الرجاء ادخال اسم المنطقة!",
+        arabicLettersOnly: "الرجاء ادخال حروف عربية فقط!",
+      },
+    },
+    // errorClass: 'custom-error',
+    highlight: function(element) {
+      $(element).closest('.row').addClass('has-error');
+    },
+    unhighlight: function(element) {
+      $(element).closest('.row').removeClass('has-error');
+    },
+  });
+
+});
