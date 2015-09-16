053436de9e48eb9033fc9321f37662fbfed8891b
abdullah Elamir <abdullah.el_ameer@yahoo.com>
2015-09-16 21:51:58 +0200

    تنظيم seqlize

---
+++ app/region.js
@@ -0,0 +1,47 @@
+var models = require("../models");
+exports.region_mgr = {
+  /* get  all regions */
+  get_regions : function(cb){
+    models.Region.findAndCountAll({
+      where: {
+        status: 1
+      }
+    }).then(function(result) {
+      cb(result);
+    });
+  },
+   /* add new region*/
+  add_region : function(body,cb){
+    models.Region.create(body).then(function(result) {
+      cb(result);
+    });
+  },
+  /* update region*/
+  edit_region : function(body,cb){
+    models.Region.update({
+      region_name: body.region_name
+    },{
+      where:{
+        id:body.id
+      }
+    }).then(function(result){
+      cb(result);
+    });
+  },
+  /*delete region  */
+  delete_region : function(body,cb){
+    models.Region.destroy({
+      where:{
+        id:body.id
+      }
+    }).then(function(result){
+      cb('/region?msg=1');
+    }).catch(function(err){
+      cb('/region?msg=2');
+    });
+  },
+
+
+
+
+};
