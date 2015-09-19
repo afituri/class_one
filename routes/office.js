9e8290f5d23b0f9c9ff91a58cca5ce4858a63ee8
abdullah Elamir <abdullah.el_ameer@yahoo.com>
2015-09-19 18:49:18 +0200

    ya

--- routes/office.js
+++ routes/office.js
@@ -1,40 +1,40 @@
 var models = require("../models");
 var office = require("../app/office")
-	.office_mgr;
+  .office_mgr;
 module.exports = function (router) {
 
-	/* GET offices page. */
-	router.get('/office', function (req, res) {
-		office.get_office(function (result) {
-			res.render('office', {
-				title: 'السجل المدني',
-				offices: result.offices.rows,
-				manucipalitys: result.manucipalitys,
-				regions: result.regions,
-				branches: result.branches
-			});
-		})
-	});
+  /* GET offices page. */
+  router.get('/office', function (req, res) {
+    office.get_office(function (result) {
+      res.render('office', {
+        title: 'السجل المدني',
+        offices: result.offices.rows,
+        manucipalitys: result.manucipalitys,
+        regions: result.regions,
+        branches: result.branches
+      });
+    })
+  });
 
-	/* Add Officees page. */
-	router.post('/office/new_office', function (req, res) {
-		office.add_office(req.body, function (result) {
-			res.redirect(result);
-		})
-	});
+  /* Add Officees page. */
+  router.post('/office/new_office', function (req, res) {
+    office.add_office(req.body, function (result) {
+      res.redirect(result);
+    })
+  });
 
-	/* Edit Offices page. */
-	router.post('/office/edit_office', function (req, res) {
-		office.update_office(req.body, function(result){
-			res.redirect(result);
-		})
-	});
+  /* Edit Offices page. */
+  router.post('/office/edit_office', function (req, res) {
+    office.update_office(req.body, function(result){
+      res.redirect(result);
+    })
+  });
 
-	/* Delete Offices page. */
-	router.post('/Office/delete_Office', function (req, res) {
-		office.delete_Office(req.body,function(result){
-			res.redirect(result);
-		})
-	});
+  /* Delete Offices page. */
+  router.post('/Office/delete_Office', function (req, res) {
+    office.delete_Office(req.body,function(result){
+      res.redirect(result);
+    })
+  });
 
 }
