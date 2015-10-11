091542051d24862ec6d06f7d7c387ec3d86e191b
abdullah Elamir <abdullah.el_ameer@yahoo.com>
2015-10-11 17:37:02 +0200

    F

---
+++ app/country.js
@@ -0,0 +1,17 @@
+var models = require("../models");
+exports.personal_mgr = {
+  get_country: function (cb) {
+    models.sequelize.query('', {
+      replacements: []
+    }).then(function () {
+
+    });
+
+
+  },
+
+
+
+
+
+  };
--- routes/personal.js
+++ routes/personal.js
@@ -3,42 +3,13 @@ var constants = require("../data/constants.json");
 module.exports = function (router) {
   /* GET personals page. */
   router.get('/personal', function (req, res) {
-    models.Member.findAndCountAll({
-      where: {
-        status: 1
-      },
-      include: [{
-        model: models.Personal,
-        where: {
-          status: 1
-        },
-        include: [{
-          model: models.Country,
-          as: 'Nationality',
-          where: {
-            status: 1
-          },
-        }]
-      } , {
-        model: models.Kinship,
-        where: {
-          status: 1
-        },
-      }]
-    }).then(function (result) {
-      models.Country.findAndCountAll({
-        where: {
-          status: 1
-        }
-      }).then(function (Country) {
+
         res.render('personal', {
         title: 'أفراد الأسرة',
         personals: result.rows,
         social: constants,
         country: Country
         });
-      });
-    });
   });
 
   /* Add personals page. */
