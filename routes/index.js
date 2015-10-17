6b3ab21d85c79c137f8248fbbc22fc9955157f5d
faisalrahil <faisallrahil@gmail.com>
2015-10-17 21:11:04 +0200

    edit

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ var office = require('./office')(router);
 var family = require('./family')(router);
 var personal = require('./personal')(router);
 var city = require('./city')(router);
+var reports = require('./reports')(router);
 
 /* GET home page. */
 router.get('/', function (req, res) {
