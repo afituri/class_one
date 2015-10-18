68654396973b85727dadbd2e36abc12b1fefb8fd
abdooageel <ageel.abdoo@gmail.com>
2015-10-18 19:18:15 +0200

    واقعة ولادة

--- routes/birth.js
+++ routes/birth.js
@@ -1,9 +1,15 @@
 var models = require("../models");
+var region = require('../app/region').region_mgr;
+var country = require('../app/country').country_mgr;
 module.exports = function (router) {
 
   router.get('/birth', function (req, res) {
-    res.render('birth', {
-      title : "افراد الاسرة" 
+    region.get_regions(function(result){
+      country.get_country(function(countrys){
+        res.render('birth', {
+        title : "افراد الاسرة" ,regions:result.rows,countrys:countrys
+        });
+      });
     });
   });
 
