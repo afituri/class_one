ab6a7b81bbef9b9336ddf6493fb5da05f8f9f940
abdooageel <ageel.abdoo@gmail.com>
2015-10-19 19:34:43 +0200

    birth add

--- routes/birth.js
+++ routes/birth.js
@@ -1,16 +1,22 @@
 var models = require("../models");
 var region = require('../app/region').region_mgr;
 var country = require('../app/country').country_mgr;
+var member = require('../app/member').member_mgr;
 module.exports = function (router) {
 
-  router.get('/birth', function (req, res) {
+  router.get('/birth/:id', function (req, res) {
     region.get_regions(function(result){
       country.get_country(function(countrys){
-        res.render('birth', {
-        title : "افراد الاسرة" ,regions:result.rows,countrys:countrys
+        member.get_family_members(req.params.id,function(fpersonals){
+          res.render('birth', {
+          title : "افراد الاسرة" ,regions:result.rows,countrys:countrys,personals:fpersonals
+          });
         });
       });
     });
   });
 
+  router.post('/birth/new_birth',function (req, res) {
+    console.log(req.body);
+  });
 }
