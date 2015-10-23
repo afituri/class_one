945c940940db6d733a942bc19606c47645e8f13d
faroukshouba <faroukshouba@gmail.com>
2015-10-23 14:32:42 +0200

    add divorce

--- public/js/divorce.js
+++ public/js/divorce.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('#Birth_Date, #Enlistingdate').datetimepicker({
+  $('#inform_date, #divorce_date').datetimepicker({
     useCurrent: false,
     viewMode: 'years',
     format: 'YYYY-MM-DD',
@@ -8,10 +8,7 @@ $(document).ready(function(){
   $("[data-toggle=tooltip]").tooltip();
   $('#weight').mask('0.000');
   $("[name='family_type']").bootstrapSwitch();
-  // $('body').on('switch-change','#mySwitch', function () {
-  //   alert("dd");
-  // });
-   
+  /*............*/
   $("#child-table").hide(0);
   $('#toggle-child').change(function() {
     if ($(this).prop('checked') == true) {
@@ -20,37 +17,84 @@ $(document).ready(function(){
     else {
       $("#child-table").hide(200);
     }
-  })
-  $('#get_region').on('change',function() {
-    var id = $('#get_region').val();
-    $.get('/get_branch_by_region_id'+id,function(result){
-      $('#get_branch').empty();
-      for ( var i = 0; i < result.length;  i++ ) {
-        $('#get_branch').append("<option value = '"+result[i].id+"'>"+result[i].branch_name+"</option>");
+  });
+  /*...........*/
+  $('body').on('change', '#region', function(){
+    var id = $(this).val();
+    $('#Branches').empty();
+    $('#Offices').empty();
+    $('#Branches').append("<option value = '' style='colr:grey; display:none;'> مكتب الاصدار</option>").selectpicker('refresh');
+    $('#Offices').append("<option value = '' style='colr:grey; display:none;'> السجل المدني</option>").selectpicker('refresh');
+    $.get('/branch/get_branch/'+id,function(data){
+      for(key in data){
+          $('#Branches').append("<option value = '"+data[key].id+"'>"+data[key].branch_name+"</option>").selectpicker('refresh');
+        }
+    });
+  });
+/*.........Branches........*/
+  $('body').on('change', '#Branches', function(){
+    var id = $(this).val();
+    $('#Offices').empty();
+    $('#Offices').selectpicker('refresh');
+    $.get('/Office/get_Office/'+id,function(data){
+      $('#Offices').append("<option value = '' style='colr:grey; display:none;'> السجل المدني</option>").selectpicker('refresh');
+      for(key in data){
+          $('#Offices').append("<option value = '"+data[key].id+"'>"+data[key].office_name+"</option>").selectpicker('refresh');
+        }
+    });
+  });
+  /*-----------------------*/
+  $('body').on('change', '#country', function(){
+    var id = $(this).val();
+    $('#Cities').empty();
+    $.get('/city/get_city/'+id,function(data){
+      for(key in data){
+          $('#Cities').append("<option value = '"+data[key].id+"'>"+data[key].city_name+"</option>").selectpicker('refresh');
+        }
+    });
+  });
+/*---------------------*/
+/*........waif......*/
+  $('#open_family').on('click', function(){
+   var id = $(this).val();
+    $.get('/divorce/'+id,function(result){
+    $('#waif_name').empty()
+    $('#father_name').append("<input type = 'hidden' name='father_name' value="+result.fathers[0][0].Person_Id+"></input><label > ="+result.fathers[0][0].Arabic_Firstname+" "+result.fathers[0][0].Arabic_Fathername+" "+result.fathers[0][0].Arabic_Grandfathername+" "+result.fathers[0][0].Arabic_Familyname+"</label>");
+    $('#waif_name').append("<option value = '' style='colr:grey; display:none;'> أسم الزوجة</option>").selectpicker('refresh');
+      for(i in result.wifes){
+        $('#waif_name').append('<option value='+result.wifes[0][i].Person_Id+'">'+result.wifes[0][i].Arabic_Firstname+' '+result.wifes[0][i].Arabic_Fathername+' '+result.wifes[0][i].Arabic_Grandfathername+' '+result.wifes[0][i].Arabic_Familyname+'</option>').selectpicker('refresh');
       }
     });
   });
-  $('#get_region').on('change', function(){
-    var id = $(this).val();
-    get_branches('#get_branch',id);
-  });
-
-  /*-------on select region edt--------*/
-  // $('#region_edit').on('change', function(){
-  //   var id = $(this).val();
-  //   get_branches('#edit_office_branch',id);
+  /*----------------*/
+  // $('#open_family').on('click', function(){
+  //  var id = $(this).val();
+  //   $.get('/divorce/'+id,function(result){
+  //   $('#waif_name').empty()
+  //   $('#waif_name').append("<option value = '' style='colr:grey; display:none;'> أسم الزوجة</option>").selectpicker('refresh');
+  //     for(i in result){
+  //       alert(result[i].Arabic_Firstname);
+  //       $('#waif_name').append('<option value='+result[i].Person_Id+'">'+result[i].Arabic_Firstname+' '+result[i].Arabic_Fathername+' '+result[i].Arabic_Grandfathername+' '+result[i].Arabic_Familyname+'</option>').selectpicker('refresh');
+  //     }
+  //   });
   // });
+  /*--------------------*/
+  /*.........dath......*/
+  $('#waif_name').on('change', function(){
+    var id = $(this).val();
+    $.get('/divorce/Personal/'+id,function(result){
+      $('#data').append('<label class="form-control">'+new Date(result[0].Birth_Date).getFullYear()+'/'+new Date(result[0].Birth_Date).getMonth()+'/'+new Date(result[0].Birth_Date).getDate()+'</label>');
+    });
+  /*..........suns........*/ 
+    $.get('/divorce/suns/'+id,function(result){
+      for (i in result){
+        $('#family_table').append("<tr><td class=text-center>"+result[i].Arabic_Firstname+" "+result[i].Arabic_Fathername+" "+result[i].Arabic_Grandfathername+" "+result[i].Arabic_Familyname+"</td><td class=text-center>"+new Date(result[i].Birth_Date).getFullYear()+'/'+new Date(result[i].Birth_Date).getMonth()+'/'+new Date(result[i].Birth_Date).getDate()+"</td><td class=text-center>"+result[i].Arabic_Motherfirstname+" "+result[i].Arabic_Motherfathername+" "+result[i].Arabic_Mothergrandfathername+" "+result[i].Arabic_Motherfamilyname+"</td><td class=text-center><p data-placement=\"top\" data-toggle=\"tooltip\" title=\"أختار\"> \
+        <input id=\"family_come_from\" type=\"checkbox\" name=\"family_come_from\" class=\"form-control\"/> \
+        </p></td></tr>");
+      }  
+    });
+  });
+  /*_________________________*/
 
-  /*------get branch function-----*/
-  function get_branches(select_id, branch_id) {
-    $(select_id).empty().selectpicker('refresh');
-    if(branch_id) {
-      $.get('/divorce/get_branch_by_region_id/'+branch_id,function(result){
-        for (key in result){
-          $(select_id).append("<option value = '"+result[key].id+"'>"+result[key].branch_name+"</option>").selectpicker('refresh');  
-        }
-      });
-    }
-  }
-  
+
 });    
--- routes/divorce.js
+++ routes/divorce.js
@@ -1,28 +1,131 @@
 var models = require("../models");
 var family = require("../app/family")
   .family_mgr;
+var divorce = require("../app/divorce")
+  .divorce_mgr;  
 // var branch = require('../app/branch')
 //   .branch_mgr;  
 var office = require("../app/office")
   .office_mgr; 
-//var region = require('../app/region').region_mgr;
+var country = require('../app/country').country_mgr;
+
+var region = require('../app/region').region_mgr;
 module.exports = function (router) {
 
   router.get('/divorce', function (req, res) {
     family.get_family(function (result) {
-      office.get_office(function (result1) {
-        res.render('divorce', {
-          title : "واقعة الطﻻق",familys: result.result.rows,
-          offices: result.offices.rows,
-          regions: result1.regions,
-          branches: result1.branches
-        });
+      region.get_regions(function(result1) {
+        country.get_country(function(countrys){
+          res.render('divorce', {
+            title : "واقعة الطﻻق",familys: result.result.rows,
+            regions:result1.rows,
+            countrys:countrys
+          });
+        })  
       })
     });  
   });
-  router.get('/divorce/get_branch_by_region_id/:id', function (req, res) {
-    branch.get_branch_by_region_id(req.params.id, function (result) {
-      res.send(result);
+  /*------------*/
+  var familyId;
+  router.get('/divorce/:id', function (req, res) {
+    familyId=req.params.id;
+      divorce.get_wife(familyId,function (wife){  
+        divorce.get_father(familyId,function (father){
+          var names = {
+          wifes : wife,
+          fathers :  father,
+          };
+          console.log(names.wifes);
+          res.send(names);
+        })
+      })
+  });
+  /*============*/
+  var sunId;
+  router.get('/divorce/suns/:id', function (req, res) {
+    sunId=req.params.id;
+      divorce.get_suns(sunId,function (suns){
+        console.log(suns[0]);
+        res.send(suns[0]);
+     })
+  });
+  /*===============*/
+  router.get('/divorce/Personal/:id', function (req, res) {
+    divorce.get_Personal(req.params.id,function (per){
+      res.send(per[0]);
+    })
+  });
+/*==================*/
+  router.post('/divorce/new_divorce',function (req, res) {
+    if(req.body.wife_custody == 'on')
+      {
+        var w_custody = 1;
+      }
+      else
+      {
+        var w_custody = 2;
+      }
+    var divorce_c = {
+      wife_custody : w_custody,
+      husband_personal_Id : req.body.father_name,
+      wife_personal_Id : req.body.waif_name,
+      wife_bt_family : req.body.wife_bt_family,
+      officeId : req.body.officeId,
+      divorce_place : req.body.divorce_place,
+      divorce_date : req.body.divorce_date,
+      contract_number : req.body.contract_divorce_number,
+      record_divorce_nu : req.body.record_divorce_number,
+      inform_date : req.body.inform_date,
+      city_Id : req.body.city_Id,
+      divorce_type : req.body.divorce_type
+    };
+    console.log(divorce_c);
+    divorce.add_divorce(divorce_c,function(result2){
     });
+  //   personal_c['Arabic_Firstname']=req.body.OfficeId;
+  //   console.log(personal_c);
+    // personal_c['Gender']=req.body.Gender;
+    // personal_c['Birth_Date']=req.body.Birth_Date;
+    // personal_c['Birth_Place']=req.body.Birth_Place;
+    // personal_c['city_Id']=req.body.city_Id;
+    // personal_c['Fatherperson_Id']=req.body.Fatherperson_Id;
+    // personal_c['Motherperson_Id']=req.body.Motherperson_Id;
+    // personal_c['Enlistingdate']=req.body.Enlistingdate;
+    // personal.add_personal(personal_c,function(result){
+    //   var members_c={};
+    //   members_c['KinshipId']=3;
+    //   members_c['PersonalId']=result.id;
+    //   members_c['FamilyId']=req.body.FamilyId;
+    //   member.add_members(members_c,function(result1){
+    //     if(req.body.newborn_reporting==2){
+    //       var id_office=req.body.OfficeIdw;
+    //     }else{
+    //       var id_office=req.body.OfficeId;
+    //     }
+    //     var birth_c={
+    //       birth_type: req.body.birth_type,
+    //       children_no:req.body.children_no ,
+    //       informer_type:req.body.informer_type ,
+    //       informer_name:req.body.informer_name ,
+    //       informer_address: req.body.informer_address,
+    //       record_no:req.body.record_no ,
+    //       record_paper_no:req.body.record_paper_no ,
+    //       place_of_birth:req.body.place_of_birth ,
+    //       newborn_type:req.body.newborn_type,
+    //       newborn_reporting: req.body.newborn_reporting,
+    //       newborn_state:req.body.newborn_state ,
+    //       birth_state: req.body.birth_state,
+    //       newborn_health: req.body.newborn_health,
+    //       pregnancy_period: req.body.pregnancy_period,
+    //       weight:req.body.weight ,
+    //       midwife_type: req.body.midwife_type,
+    //       midwife_name: req.body.midwife_name,
+    //       PersonalId:result.id,
+    //       OfficeId:id_office
+    //     }
+    //     birth.add_birth(birth_c,function(result2){
+    //     });
+    //   });
+    // });  
   });
 }
