ed8352a29c8931f55339d7538fa9b3246d7288e4
Aladdin Sonni <alaaden3000@gmail.com>
2015-10-27 14:54:24 +0200

    add validation function in login.jade

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,39 @@
 $(document).ready(function(){
-
+  // Validation function start
+  /*----------- validate in login form----------*/
+  $("#login_form").validate({
+    rules:{
+      user_email:{
+        required: true,
+        email: true
+      },
+      user_password:{
+        required: true,
+      },
+    },
+    messages:{
+      user_email:{
+        required: "",
+        email: "خطأ: الرجاء ادخال بريد الكتروني صالح!"
+      },
+      user_password:{
+        required: "",
+      }
+    },
+    errorClass: 'custom-error',
+    highlight: function(element) {
+      $(element).closest('.form-group').addClass('has-error').removeClass('has-success');
+    },
+    unhighlight: function(element) {
+      $(element).closest('.form-group').removeClass('has-error').addClass('has-success');
+    },
+    invalidHandler: function(event, validator) {
+      // 'this' refers to the form
+      var errors = validator.numberOfInvalids();
+      if (errors) {
+        custNotify("danger","خطأ","الرجاء ادخال اسم المستخدم وكلمة المرور","ok-sign","bounceIn","bounceOut");
+      }
+    }
+  });
+  // Validation function end
 });
