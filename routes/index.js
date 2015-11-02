b47275b223dbdb1071d0772490caf881d26fa55f
abdullah Elamir <abdullah.el_ameer@yahoo.com>
2015-11-02 16:36:58 +0200

    working in report

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ var personal = require('./personal')(router);
 var city = require('./city')(router);
 var reports = require('./reports')(router);
 var report_family = require('./report_family')(router);
+var report_personal = require('./report_personal')(router);
 var deathreason = require('./deathreason')(router);
 var birth = require('./birth')(router);
 var death = require('./death')(router);
